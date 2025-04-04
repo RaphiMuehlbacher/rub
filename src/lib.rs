117b85c7e96967a3c34e5c1df9f667ddf56a3ba5
RaphiMuehlbacher <raphi.muehlbacher@gmail.com>
2025-04-04 21:51:58 +0200

    refactor: Token consists of TokenKind, position, literal, lexeme

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use miette::{miette, LabeledSpan, Report};
 
-#[derive(Debug, Copy, Clone, PartialEq)]
-pub enum Token{
+#[derive(Debug, Copy, Clone)]
+pub enum TokenKind {
     LeftParen,
     RightParen,
     LeftBrace,
@@ -23,12 +23,20 @@ pub enum Token{
     LessEqual,
     Question,
     Colon,
+    EOF,
 }
 
+#[derive(Debug, Copy, Clone)]
+pub struct Token<'a> {
+    token_kind: TokenKind,
+    position: usize,
+    lexeme: &'a str,
+    literal: &'a str
+}
 
 pub struct Lexer<'a>{
     source: &'a String,
-    tokens: Vec<Result<Token, Report>>,
+    tokens: Vec<Result<Token<'a>, Report>>,
     position: usize,
     start: usize,
 }
@@ -37,36 +45,49 @@ impl<'a> Lexer<'a> {
     pub fn new(source: &'a String) -> Self {
         Lexer{source, tokens: vec![], position: 0, start: 0}
     }
-    pub fn lex(&mut self) -> &Vec<Result<Token, Report>> {
-        let mut chars = self.source.chars();
+    pub fn lex(&mut self) -> &Vec<Result<Token<'a>, Report>> {
+        while self.position < self.source.len() {
+            self.start = self.position;
 
-        for char in chars {
-            let token = match char {
-                '(' => Ok(Token::LeftParen),
-                ')' => Ok(Token::RightParen),
-                '{' => Ok(Token::LeftBrace),
-                '}' => Ok(Token::RightBrace),
-                ',' => Ok(Token::Comma),
-                '.' => Ok(Token::Dot),
-                '-' => Ok(Token::Minus),
-                '+' => Ok(Token::Plus),
-                ';' => Ok(Token::Semicolon),
-                '/' => Ok(Token::Slash),
-                '*' => Ok(Token::Star),
-                '!' => Ok(Token::Bang),
-                _ => {
-                    Err(miette!(
+            if let Some(c) = self.source[self.position..].chars().next(){
+                self.position += c.len_utf8();
+
+                let token = match c {
+                    '(' => Ok(self.just(TokenKind::LeftParen)),
+                    ')' => Ok(self.just(TokenKind::RightParen)),
+                    '{' => Ok(self.just(TokenKind::LeftBrace)),
+                    '}' => Ok(self.just(TokenKind::RightBrace)),
+                    ',' => Ok(self.just(TokenKind::Comma)),
+                    '.' => Ok(self.just(TokenKind::Dot)),
+                    '-' => Ok(self.just(TokenKind::Minus)),
+                    '+' => Ok(self.just(TokenKind::Plus)),
+                    ';' => Ok(self.just(TokenKind::Semicolon)),
+                    '/' => Ok(self.just(TokenKind::Slash)),
+                    '*' => Ok(self.just(TokenKind::Star)),
+                    '!' => Ok(self.just(TokenKind::Bang)),
+                    _ => {
+                        Err(miette!(
                         help = "unexpected character",
-                        labels = vec![LabeledSpan::at(self.position, "here")],
+                        labels = vec![LabeledSpan::at(self.start, format!("unexpected '{}'", c))],
                         "expected closing ')'"
                         )
-                        .with_source_code(self.source.to_string()))
+                            .with_source_code(self.source.to_string()))
 
-                }
-            };
-            self.tokens.push(token);
-            self.position += 1;
+                    }
+                };
+                self.tokens.push(token);
+            } else {
+                eprintln!("I don't know");
+            }
         }
+        let eof_token = Token{token_kind: TokenKind::EOF, position: self.source.len(), literal: "", lexeme: "", };
+        self.tokens.push(Ok(eof_token));
+
         &self.tokens
+    }
+
+    pub fn just(&self, token_kind: TokenKind) -> Token<'a> {
+        let lexeme = &self.source[self.start..self.position];
+        Token {token_kind, position: self.start, lexeme, literal: lexeme}
     }
 }
