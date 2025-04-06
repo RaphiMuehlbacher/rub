77693d014664babb689023b96e882799a067f971
RaphiMuehlbacher <raphi.muehlbacher@gmail.com>
2025-04-06 21:07:41 +0200

    implemented custom errors

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,29 @@
 use std::fs;
 use miette::{Report};
-use rslox::Lexer;
+use rslox::{Lexer};
 
 fn main() -> Result<(), Report> {
     let mut path = "source.lox".to_string();
     let source = fs::read_to_string(&mut path).expect(format!("Error reading file {}", path).as_str());
 
     let mut lexer = Lexer::new(source.as_str());
-    let tokens = lexer.lex();
+    let tokens_results = lexer.lex();
+    let mut tokens = Vec::new();
+    let mut errors = Vec::new();
 
-    for token_result in tokens {
-        match token_result {
-            Ok(token) => {
-                println!("{:?}", token);
-            }
-            Err(err) => {
-                eprintln!("{:?}", err);
-            }
+    for res in tokens_results {
+        match res {
+            Ok(token) => tokens.push(token.clone()),
+            Err(err) => errors.push(err),
         }
+    }
+
+    for err in errors {
+        eprintln!("Lexing error: {:?}", err);
+    }
+
+    for token in tokens {
+        println!("{:?}", token);
     }
 
     Ok(())
