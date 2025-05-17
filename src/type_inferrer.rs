694618d8b00adaecb312847a5cc6d2de074f88f6
RaphiMuehlbacher <raphi.muehlbacher@gmail.com>
2025-05-17 12:00:59 +0200

    fixed function and lambda declarations

--- src/type_inferrer.rs
+++ src/type_inferrer.rs
@@ -248,13 +248,11 @@ impl<'a> TypeInferrer<'a> {
         self.current_function_return_ty = Some(fun_decl.node.return_type.clone());
 
         self.infer_stmts(&fun_decl.node.body.node.statements)?;
-        let body_ty = if let Some(expr) = &fun_decl.node.body.node.expr {
-            self.infer_expr(expr)?
-        } else {
-            Type::Nil
-        };
 
-        self.unify(fun_decl.node.return_type.clone(), body_ty, fun_decl.node.ident.span)?;
+        if let Some(expr) = &fun_decl.node.body.node.expr {
+            let body_ty = self.infer_expr(expr)?;
+            self.unify(fun_decl.node.return_type.clone(), body_ty, fun_decl.node.ident.span)?;
+        }
 
         self.current_function_return_ty = old_ret_ty;
         self.var_env.pop();
@@ -476,8 +474,10 @@ impl<'a> TypeInferrer<'a> {
                 let old_ret_ty = self.current_function_return_ty.clone();
                 self.current_function_return_ty = Some(lambda.return_type.clone());
 
-                for stmt in &lambda.body.node.statements {
-                    self.infer_stmt(stmt)?;
+                self.infer_stmts(&lambda.body.node.statements)?;
+                if let Some(expr) = &lambda.body.node.expr {
+                    let body_ty = self.infer_expr(expr)?;
+                    self.unify(lambda.return_type.clone(), body_ty, lambda.body.span)?;
                 }
 
                 self.current_function_return_ty = old_ret_ty;
