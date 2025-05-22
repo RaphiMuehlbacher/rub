91118b89b418c4b55c2b3b0458a504fef77c66c2
RaphiMuehlbacher <raphi.muehlbacher@gmail.com>
2025-05-22 18:48:48 +0200

    preparing for merge

--- src/type_inferrer.rs
+++ src/type_inferrer.rs
@@ -19,20 +19,14 @@ pub enum Type {
     Bool,
     String,
     Nil,
-    Function { params: Vec<Box<Type>>, return_ty: Box<Type> },
+    Function { params: Vec<Type>, return_ty: Box<Type> },
     TypeVar(TypeVarId),
     Generic(String),
 }
 
 #[derive(Debug, Clone, PartialEq)]
-pub enum TypeScheme {
-    Mono(Type),
-    Poly(Vec<TypeVarId>, Type),
-}
-
-#[derive(Debug, Clone, PartialEq)]
 pub struct VarEnv {
-    scopes: Vec<HashMap<String, TypeScheme>>,
+    scopes: Vec<HashMap<String, TypeVarId>>,
 }
 
 impl VarEnv {
@@ -50,14 +44,14 @@ impl VarEnv {
         self.scopes.pop();
     }
 
-    pub fn insert(&mut self, name: String, ty: TypeScheme) {
+    pub fn insert(&mut self, name: String, ty: TypeVarId) {
         self.scopes.last_mut().unwrap().insert(name, ty);
     }
 
-    pub fn lookup(&mut self, name: &str) -> Option<TypeScheme> {
+    pub fn lookup(&mut self, name: &str) -> Option<TypeVarId> {
         for scope in self.scopes.iter().rev() {
-            if let Some(scheme) = scope.get(name) {
-                return Some(scheme.clone());
+            if let Some(id) = scope.get(name) {
+                return Some(*id);
             }
         }
         None
@@ -102,75 +96,6 @@ impl<'a> TypeInferrer<'a> {
         typed.type_id
     }
 
-    fn generalize(&mut self, ty: &Type) -> TypeScheme {
-        let free_vars = self.free_vars(ty);
-
-        if free_vars.is_empty() {
-            return TypeScheme::Mono(ty.clone());
-        }
-
-        TypeScheme::Poly(free_vars, ty.clone())
-    }
-
-    fn bound_vars(&mut self, ty: &Type) -> Vec<TypeVarId> {
-        match ty {
-            Type::Float | Type::Bool | Type::String | Type::Nil | Type::Generic(_) => vec![],
-            Type::Function { params, return_ty } => {
-                let mut vars = vec![];
-                for param in params {
-                    vars.append(&mut self.bound_vars(param));
-                }
-                vars.append(&mut self.bound_vars(return_ty));
-                vars
-            }
-            TypeVar(id) => vec![*id],
-        }
-    }
-
-    fn free_vars(&mut self, ty: &Type) -> Vec<TypeVarId> {
-        match ty {
-            Type::Float | Type::Bool | Type::String | Type::Nil | Type::Generic(_) => vec![],
-            Type::Function { params, return_ty } => {
-                let mut vars = vec![];
-                for param in params {
-                    vars.append(&mut self.free_vars(param));
-                }
-                vars.append(&mut self.free_vars(return_ty));
-                vars
-            }
-            TypeVar(id) => {
-                // First get the bound type (if any) to avoid multiple borrows
-                let bound_ty = self.type_env.get(id).cloned();
-
-                match bound_ty {
-                    Some(ty) => {
-                        if let TypeVar(bound_id) = ty {
-                            if bound_id == *id { vec![*id] } else { self.free_vars(&ty) }
-                        } else {
-                            self.free_vars(&ty)
-                        }
-                    }
-                    None => vec![*id],
-                }
-            }
-        }
-    }
-
-    fn instantiate(&mut self, scheme: &TypeScheme) -> Type {
-        match scheme {
-            TypeScheme::Mono(ty) => ty.clone(),
-            TypeScheme::Poly(type_vars, ty) => {
-                let mut substitutions = HashMap::new();
-
-                for &var_id in type_vars {
-                    substitutions.insert(var_id, TypeVar(self.fresh_type_var()));
-                }
-
-                self.substitute(ty, &substitutions)
-            }
-        }
-    }
-
     fn substitute(&self, ty: &Type, substitutions: &HashMap<TypeVarId, Type>) -> Type {
         match ty {
             Type::Float | Type::Bool | Type::String | Type::Nil | Type::Generic(_) => ty.clone(),
@@ -194,13 +119,15 @@ impl<'a> TypeInferrer<'a> {
     }
 
     fn unify(&mut self, left_ty: Type, right_ty: Type, span: SourceSpan) -> Result<Type, TypeInferrerError> {
+        let left_ty = self.lookup_type(&left_ty);
+        let right_ty = self.lookup_type(&right_ty);
+
         match (left_ty, right_ty) {
             (Type::Float, Type::Float) => Ok(Type::Float),
             (Type::String, Type::String) => Ok(Type::String),
             (Type::Bool, Type::Bool) => Ok(Type::Bool),
             (Type::Nil, Type::Nil) => Ok(Type::Nil),
-            (Type::Generic(name1), Type::Generic(name2)) if name1 == name2 => Ok(Type::Generic(name1)),
-            (Type::Generic(name1), t2) | (t2, Type::Generic(name1)) => Ok(t2),
+
             (Type::Function { params: p1, return_ty: r1 }, Type::Function { params: p2, return_ty: r2 }) => {
                 if p1.len() != p2.len() {
                     return Err(TypeMismatch {
@@ -212,7 +139,7 @@ impl<'a> TypeInferrer<'a> {
                 }
 
                 for (param1, param2) in p1.iter().zip(p2.iter()) {
-                    self.unify(*param1.clone(), *param2.clone(), span)?;
+                    self.unify(param1.clone(), param2.clone(), span)?;
                 }
 
                 self.unify(*r1.clone(), *r2, span)?;
@@ -220,18 +147,6 @@ impl<'a> TypeInferrer<'a> {
             }
 
             (ty, TypeVar(id)) | (TypeVar(id), ty) => {
-                // Check for recursive types
-                if let Type::TypeVar(_) = ty {
-                    // Allow unifying two type variables
-                } else if self.occurs_check(id, &ty) {
-                    return Err(TypeMismatch {
-                        src: self.source.clone(),
-                        span,
-                        expected: TypeVar(id),
-                        found: ty,
-                    });
-                }
-
                 self.type_env.insert(id, ty.clone());
                 Ok(TypeVar(id))
             }
@@ -245,14 +160,24 @@ impl<'a> TypeInferrer<'a> {
         }
     }
 
-    fn occurs_check(&self, id: TypeVarId, ty: &Type) -> bool {
+    pub fn lookup_type(&mut self, ty: &Type) -> Type {
         match ty {
-            Type::Float | Type::Bool | Type::String | Type::Nil | Type::Generic(_) => false,
-            Type::Function { params, return_ty } => params.iter().any(|p| self.occurs_check(id, p)) || self.occurs_check(id, return_ty),
-            TypeVar(check_id) => *check_id == id,
+            TypeVar(id) => {
+                if let Some(inner) = self.type_env.get(id).cloned() {
+                    let resolved = self.lookup_type(&inner);
+                    self.type_env.insert(*id, resolved.clone());
+                    resolved
+                } else {
+                    ty.clone()
+                }
+            }
+            // Type::Vec(elem_ty) => {
+            //     let resolved_elem = self.lookup_type(elem_ty);
+            //     Type::Vec(Box::new(resolved_elem))
+            // }
+            _ => ty.clone(),
         }
     }
-
     pub fn infer(&mut self) -> TypeInferenceResult {
         self.declare_native_functions();
 
@@ -276,13 +201,18 @@ impl<'a> TypeInferrer<'a> {
             params: vec![],
             return_ty: Box::new(Type::Float),
         };
-        self.var_env.insert("clock".to_string(), TypeScheme::Mono(clock_type));
+
+        let clock_type_id = self.fresh_type_var();
+        self.type_env.insert(clock_type_id, clock_type);
+        self.var_env.insert("clock".to_string(), clock_type_id);
 
         let print_type = Type::Function {
-            params: vec![Box::new(Type::String)],
+            params: vec![Type::String],
             return_ty: Box::new(Type::Nil),
         };
-        self.var_env.insert("print".to_string(), TypeScheme::Mono(print_type));
+        let print_type_id = self.fresh_type_var();
+        self.type_env.insert(print_type_id, print_type);
+        self.var_env.insert("print".to_string(), print_type_id);
     }
 
     fn declare_stmt(&mut self, stmt: &Stmt) {
@@ -291,72 +221,14 @@ impl<'a> TypeInferrer<'a> {
                 let name = &fun_decl.node.ident.node;
 
                 let fn_type = Type::Function {
-                    params: fun_decl
-                        .node
-                        .params
-                        .iter()
-                        .map(|p| Box::new(p.node.type_annotation.clone()))
-                        .collect(),
+                    params: fun_decl.node.params.iter().map(|p| p.node.type_annotation.clone()).collect(),
                     return_ty: Box::new(fun_decl.node.return_type.clone()),
                 };
 
-                // Create a type scheme based on whether the function has generic parameters
-                let type_scheme = if fun_decl.node.generics.is_empty() {
-                    TypeScheme::Mono(fn_type)
-                } else {
-                    // Collect generic type variables
-                    let generic_vars = fun_decl.node.generics.iter().map(|_| self.fresh_type_var()).collect();
-                    TypeScheme::Poly(generic_vars, fn_type)
-                };
-
-                self.var_env.insert(name.clone(), type_scheme);
+                self.type_env.insert(fun_decl.node.ident.type_id, fn_type);
+                self.var_env.insert(name.clone(), fun_decl.node.ident.type_id);
             }
             _ => {}
-        }
-    }
-
-    fn lookup_var(&mut self, name: &str) -> Option<Type> {
-        if let Some(scheme) = self.var_env.lookup(name) {
-            return Some(self.instantiate(&scheme));
-        }
-        None
-    }
-
-    // Update the lookup_type method to handle recursive lookups better
-    fn lookup_type(&self, type_var: &Type) -> Type {
-        let mut visited = HashSet::new();
-        self.lookup_type_internal(type_var, &mut visited)
-    }
-
-    fn lookup_type_internal(&self, type_var: &Type, visited: &mut HashSet<TypeVarId>) -> Type {
-        match type_var {
-            TypeVar(id) => {
-                if visited.contains(id) {
-                    // Prevent infinite recursion
-                    return type_var.clone();
-                }
-
-                visited.insert(*id);
-
-                if let Some(ty) = self.type_env.get(id) {
-                    let resolved = self.lookup_type_internal(ty, visited);
-                    visited.remove(id);
-                    resolved
-                } else {
-                    type_var.clone()
-                }
-            }
-            Type::Function { params, return_ty } => {
-                let new_params = params.iter().map(|p| Box::new(self.lookup_type_internal(p, visited))).collect();
-
-                let new_return = self.lookup_type_internal(return_ty, visited);
-
-                Type::Function {
-                    params: new_params,
-                    return_ty: Box::new(new_return),
-                }
-            }
-            _ => type_var.clone(),
         }
     }
 
@@ -378,87 +250,28 @@ impl<'a> TypeInferrer<'a> {
     }
 
     fn infer_var_decl(&mut self, var_decl: &Typed<VarDeclStmt>) -> Result<(), TypeInferrerError> {
-        let var_id = self.fresh_type_var();
-        self.type_env.insert(var_decl.node.ident.type_id, TypeVar(var_id));
-        self.var_env
-            .insert(var_decl.node.ident.node.clone(), TypeScheme::Mono(TypeVar(var_id)));
+        let var_decl_id = var_decl.node.ident.type_id.clone();
+        self.var_env.insert(var_decl.node.ident.node.clone(), var_decl_id)
 
         if let Some(init) = &var_decl.node.initializer {
             let init_type = self.infer_expr(init)?;
-            self.unify(TypeVar(var_id), init_type, var_decl.node.ident.span)?;
+            self.unify(TypeVar(var_decl_id), init_type, var_decl.node.ident.span)?;
         }
 
         Ok(())
     }
 
     fn infer_fun_decl(&mut self, fun_decl: &Typed<FunDeclStmt>) -> Result<(), TypeInferrerError> {
-        // Create a mapping of generic parameter names to type variables
-        let mut generic_map = HashMap::new();
-        for generic in &fun_decl.node.generics {
-            let var_id = self.fresh_type_var();
-            generic_map.insert(generic.node.clone(), Type::Generic(generic.node.clone()));
-        }
-
+        Ok(())
+    }
+    fn infer_stmts(&mut self, stmts: &Vec<Stmt>) -> Result<(), TypeInferrerError> {
         self.var_env.enter_scope();
 
-        // Process parameters with improved generic handling
-        let mut param_types = vec![];
-        for param in &fun_decl.node.params {
-            let param_type = match &param.node.type_annotation {
-                Type::Generic(name) => generic_map.get(name).cloned().unwrap_or_else(|| {
-                    let var_id = self.fresh_type_var();
-                    Type::TypeVar(var_id)
-                }),
-                _ => param.node.type_annotation.clone(),
-            };
-
-            param_types.push(Box::new(param_type.clone()));
-            self.var_env.insert(param.node.name.node.clone(), TypeScheme::Mono(param_type));
-        }
-
-        // Handle return type
-        let return_type = match &fun_decl.node.return_type {
-            Type::Generic(name) => generic_map.get(name).cloned().unwrap_or_else(|| {
-                let var_id = self.fresh_type_var();
-                Type::TypeVar(var_id)
-            }),
-            _ => fun_decl.node.return_type.clone(),
-        };
-
-        let old_ret_ty = self.current_function_return_ty.take();
-        self.current_function_return_ty = Some(return_type.clone());
-
-        // Process body
-        for stmt in &fun_decl.node.body.node.statements {
+        for stmt in stmts {
             self.infer_stmt(stmt)?;
         }
 
-        // Create function type with proper generic handling
-        let fn_type = Type::Function {
-            params: param_types,
-            return_ty: Box::new(return_type),
-        };
-
-        // Create appropriate type scheme
-        let type_scheme = if !fun_decl.node.generics.is_empty() {
-            let generic_vars: Vec<TypeVarId> = generic_map
-                .values()
-                .filter_map(|t| match t {
-                    Type::TypeVar(id) => Some(*id),
-                    _ => None,
-                })
-                .collect();
-            TypeScheme::Poly(generic_vars, fn_type.clone())
-        } else {
-            TypeScheme::Mono(fn_type.clone())
-        };
-
-        self.type_env.insert(fun_decl.node.ident.type_id, fn_type);
-        self.var_env.insert(fun_decl.node.ident.node.clone(), type_scheme);
-
-        self.current_function_return_ty = old_ret_ty;
         self.var_env.exit_scope();
-
         Ok(())
     }
 
@@ -468,6 +281,7 @@ impl<'a> TypeInferrer<'a> {
         for stmt in &block.node.statements {
             self.infer_stmt(stmt)?;
         }
+
 
         self.var_env.exit_scope();
         Ok(())
@@ -551,14 +365,7 @@ impl<'a> TypeInferrer<'a> {
                         match (&left_ty, &right_ty) {
                             (Type::Float, Type::Float) => Type::Float,
                             (Type::String, Type::String) => Type::String,
-                            (Type::Generic(name1), Type::Generic(name2)) if name1 == name2 => {
-                                // Return the same generic type for matching generics
-                                left_ty
-                            }
-                            _ => {
-                                // Try to unify the types
-                                self.unify(left.clone(), right.clone(), binary_expr.right.span)?
-                            }
+                            _ => self.unify(left.clone(), right.clone(), binary_expr.right.span)?,
                         }
                     }
                     BinaryOp::Star | BinaryOp::Minus | BinaryOp::Slash => {
